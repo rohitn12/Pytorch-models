bb78239a9baf9c81daa22eea8d060bbda47f990b
rohitn12 <33992841+rohitn12@users.noreply.github.com>
2018-07-25 19:29:25 -0500

    adding rnn and birnn

---
+++ biRNN.py
@@ -0,0 +1,13 @@
+class BiRNN(nn.Module):
+    def __init__(self, input_size , hidden_size , num_layers  , num_class):
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size , hidden_size , num_layers , batch_first = True , bidirectional = True)
+        self.fc = nn.Linear(hidden_size , num_class)
+    
+    def forward(self , x):
+        h0 = torch.zeros(self.num_layers *2 , x.size(0) , self.hidden_size *2)
+        c0 = torch.zeros(self.num_layers*2  ,  x.size(0) , self.hidden_size*2)
+        out , _ = self.lstm(x , (h0 , c0))
+        out = self.fc(out [:,-1:])
+        return out
---
+++ rnn.py
@@ -0,0 +1,14 @@
+class RNN(nn.Module):
+    def __init(self , input_size , hidden_layers, num_layers, num_classes  ):
+        super(RNN , self).__init__()
+        self.hidden_layers = hidden_layers
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size , hidden_layers , num_layers , batch_first = True)
+        self.fc = nn.Linear(hidden_layers , num_classes)
+        
+    def forward(self ,x):
+        h0 = torch.zeros(self.num_layers , x.size(0) , self.hidden_layers)
+        c0 = torch.zeros(self,num_layers , x.size(0) , self.hidden_layers)
+        out , _ = self.lstm (x , (h0 , c0))
+        out = self.fc(out[:,-1,:])
+        return out
